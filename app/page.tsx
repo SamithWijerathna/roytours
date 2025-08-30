bc46c58fcd62650980e8ffe593a87dd3554363a3
samithwijerathna <0wsamithaw0@gmail.com>
2025-08-30 14:43:47 +0530

    main commit

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,9 @@
 import Image from "next/image";
-
+import Header from "./components/header";
 export default function Home() {
   return (
     <div>
-      
+      <Header></Header>
     </div>
   );
 }
