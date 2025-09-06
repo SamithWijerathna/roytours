4cc67ccb23bb28ee10bc1e57f3cc269f107b4bf1
DamiduH <damiduherath@gmail.com>
2025-09-07 00:04:05 +0530

    experiences section

---
+++ app/Experiences/page.tsx
@@ -0,0 +1,66 @@
+import Header from "../components/header";
+
+export default function Experiences() {
+  return (
+    <div className="min-h-screen flex flex-col gap-8 text-[#1e1e1e]">
+      <Header />
+      <section className="p-4 pt-24 flex flex-col gap-12">
+        <div className="flex flex-col items-center text-center justify-center py-20">
+          <h1 className="text-4xl md:text-5xl font-semibold mb-6 lg:mb-2">
+            Choose Your Sri Lankan Adventure
+          </h1>
+          <p className="text-base md:text-xl lg:w-[60%]">
+            Whether you’re seeking cultural depth, scenic escapes, or thrilling
+            adventures, we have a journey tailored for you. Explore our
+            carefully crafted tours, or let us design something unique just for
+            you.
+          </p>
+        </div>
+        <div className="flex flex-col items-center lg:items-start lg:text-left text-center justify-center">
+          <h2 className="text-3xl md:text-3xl font-semibold mb-2 lg:mb-2">
+            Cultural Treasures Tour
+          </h2>
+          <p className="text-base md:text-xl w-[75%]">
+            Whether you’re seeking cultural depth, scenic escapes, or thrilling
+            adventures, we have a journey tailored for you. Explore our
+            carefully crafted tours, or let us design something unique just for
+            you.
+          </p>
+        </div>
+        <div className="flex flex-col items-center lg:items-start lg:text-left text-center justify-center">
+          <h2 className="text-3xl md:text-3xl font-semibold mb-2 lg:mb-2">
+            Cultural Treasures Tour
+          </h2>
+          <p className="text-base md:text-xl w-[75%]">
+            Whether you’re seeking cultural depth, scenic escapes, or thrilling
+            adventures, we have a journey tailored for you. Explore our
+            carefully crafted tours, or let us design something unique just for
+            you.
+          </p>
+        </div>
+        <div className="flex flex-col items-center lg:items-start lg:text-left text-center justify-center">
+          <h2 className="text-3xl md:text-3xl font-semibold mb-2 lg:mb-2">
+            Cultural Treasures Tour
+          </h2>
+          <p className="text-base md:text-xl w-[75%]">
+            Whether you’re seeking cultural depth, scenic escapes, or thrilling
+            adventures, we have a journey tailored for you. Explore our
+            carefully crafted tours, or let us design something unique just for
+            you.
+          </p>
+        </div>
+        <div className="flex flex-col items-center lg:items-start lg:text-left text-center justify-center">
+          <h2 className="text-3xl md:text-3xl font-semibold mb-2 lg:mb-2">
+            Cultural Treasures Tour
+          </h2>
+          <p className="text-base md:text-xl w-[75%]">
+            Whether you’re seeking cultural depth, scenic escapes, or thrilling
+            adventures, we have a journey tailored for you. Explore our
+            carefully crafted tours, or let us design something unique just for
+            you.
+          </p>
+        </div>
+      </section>
+    </div>
+  );
+}
--- app/components/footer.tsx
+++ app/components/footer.tsx
@@ -1,57 +1,71 @@
-import { div } from "framer-motion/client"
-import Link from "next/link"
-import Image from "next/image"
-export default function footer(){
-    return(
-        <div>
-        <footer className=" bg-[#1e1e1e] text-white hidden sm:block">
-            <div className="flex flex-row justify-between w-full items-end px-[50px]">
-                <h2 className="text-6xl font-semibold w-1/2">Let’s Plan Your Journey Together</h2>
-                <p className="font-normal w-1/2 text-xl">Got a question or ready to book your adventure? We’d love to hear from you.</p>
-            </div>
-            <div className="flex justify-between items-center px-[50px]">
-                <div className="w-1/2 py-20">
-                    <Image src={'/image/logo-white.png'} width={270} height={70} alt="Roy Tours Logo"></Image>
-                </div>
-                
-                <div className="w-1/2 text-xl py-20">
-                    <h4 className="font-semibold text-gray-400">Quick Links</h4>
-                    <ul className="flex gap-4">
-                    <li>
-                    <Link href="/">Home</Link>
-                    </li>
-                    <li>
-                    <Link href="/about">Our Story</Link>
-                    </li>
-                    <li>
-                    <Link href="/contact">Experiences</Link>
-                    </li>
-                    <li>
-                    <Link href="/contact">Destinations</Link>
-                    </li>
-                    <li>
-                    <Link href="/contact">Gallery</Link>
-                    </li>
-                </ul>
-                </div>
-            </div>
-            <div className="border-t-1 w-full text-sm flex flex-col lg:flex-row lg:justify-center text-center items-center gap-2">
-            <p>
-                All Rights Reserved By Roy Tours.{" "}
-                <span className="hidden lg:inline">|</span>
-            </p>
-            <span className="flex items-center gap-2 py-2">
-                Developed By
-                <a href="https://www.cloudwave.asia"><img
+import { div } from "framer-motion/client";
+import Link from "next/link";
+import Image from "next/image";
+export default function footer() {
+  return (
+    <div>
+      <footer className=" bg-[#1e1e1e] text-white hidden sm:block pt-8">
+        <div className="flex flex-row justify-between w-full items-end px-[50px]">
+          <h2 className="text-6xl font-semibold w-1/2">
+            Let’s Plan Your Journey Together
+          </h2>
+          <p className="font-normal w-1/2 text-xl">
+            Got a question or ready to book your adventure? We’d love to hear
+            from you.
+          </p>
+        </div>
+        <div className="flex justify-between items-center px-[50px]">
+          <div className="w-1/2 py-20">
+            <Image
+              src={"/image/logo-white.png"}
+              width={270}
+              height={70}
+              alt="Roy Tours Logo"
+            ></Image>
+          </div>
+
+          <div className="w-1/2 text-xl py-20">
+            <h4 className="font-semibold text-gray-400">Quick Links</h4>
+            <ul className="flex text-nowrap gap-4">
+              <li>
+                <Link href="/">Home</Link>
+              </li>
+              <li>
+                <Link href="/about">Our Story</Link>
+              </li>
+              <li>
+                <Link href="/Experiences">Experiences</Link>
+              </li>
+              <li>
+                <Link href="/contact">Destinations</Link>
+              </li>
+              <li>
+                <Link href="/contact">Gallery</Link>
+              </li>
+              <li>
+                <Link href="/contact">Contact Us</Link>
+              </li>
+            </ul>
+          </div>
+        </div>
+        <div className="border-t-1 w-full text-sm flex flex-col lg:flex-row lg:justify-center text-center items-center gap-2">
+          <p>
+            All Rights Reserved By Roy Tours.{" "}
+            <span className="hidden lg:inline">|</span>
+          </p>
+          <span className="flex items-center gap-2 py-2">
+            Developed By
+            <a href="https://www.cloudwave.asia">
+              <img
                 src="\image\cloud wave.png"
                 alt="Developer Logo"
                 className="h-[15px]"
-                /></a>
-                {" "}
-            </span>
-            </div>
-        </footer>
-        <footer className="p-4 bg-[#1e1e1e] text-white text-center items-center flex flex-col sm:hidden block">
+              />
+            </a>{" "}
+          </span>
+        </div>
+      </footer>
+      <footer className="p-4 bg-[#1e1e1e] text-white text-center items-center flex flex-col sm:hidden block">
         <h1 className="text-4xl font-semibold mb-2">
           Let’s Plan Your Journey Together
         </h1>
@@ -69,13 +83,16 @@ export default function footer(){
               <Link href="/about">Our Story</Link>
             </li>
             <li>
-              <Link href="/contact">Experiences</Link>
+              <Link href="/Experiences">Experiences</Link>
             </li>
             <li>
               <Link href="/contact">Destinations</Link>
             </li>
             <li>
               <Link href="/contact">Gallery</Link>
+            </li>
+            <li>
+              <Link href="/contact">Contact Us</Link>
             </li>
           </ul>
         </div>
@@ -101,7 +118,6 @@ export default function footer(){
           </span>
         </div>
       </footer>
-        </div>
-        
-    )
+    </div>
+  );
 }
